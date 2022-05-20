69ee42e58d21851ebbcca4c248c7066caf75accc
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2022-05-20 16:00:18 -0300

    Prática 1 bloco 5 dia 1

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02/script.js
@@ -0,0 +1,3 @@
+corPai = document.querySelector("#elementoOndeVoceEsta").parentNode;
+corPai.style.color = 'green';
+document.getElementById("primeiroFilhoDoFilho").innerText = 'primeiro filho do filho';
