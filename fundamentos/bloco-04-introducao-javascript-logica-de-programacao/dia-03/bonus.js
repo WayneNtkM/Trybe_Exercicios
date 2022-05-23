5c78b2a78b6f6335632c9d2d78283fe058da2cb8
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2022-05-23 12:33:49 -0300

    Bônus incompleto

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03/bonus.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03/bonus.js
@@ -32,18 +32,4 @@
 var n3 = 5;
 var aux3 = "";
 
-for (var l = 1; l <=n3; l += 1) {
-        for (var c = 1; c <= n3; c += 1) {
-            if (l === 0 || l === n3 - 1) {
-                aux3 += "*";
-            } else {
-                if (c === 0 || c === n3 - 1) {
-                    aux3 += " ";
-                } else {
-                    aux3 += " ";
-                }
-            }
-    }
-    aux3 += "\n";
-}
-console.log(aux3)
+for ()
